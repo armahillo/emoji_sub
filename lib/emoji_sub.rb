b7b30d533390836441f3959708aa673959d005ba
Aaron Hill <armahillo@gmail.com>
2020-08-22 17:35:25 -0400

    Now supports combining emoji unicodes

--- lib/emoji_sub.rb
+++ lib/emoji_sub.rb
@@ -5,7 +5,7 @@ module EmojiSub
   EMOJI_MAPPING_YAML = "data/emoji.yaml"
   
   def emoji_sub(text, additional_mappings = {})
-    known_emoji = YAML.load(File.read(EMOJI_MAPPING_YAML)).merge(additional_mappings)
+    known_emoji = emoji_definitions.merge(additional_mappings)
 
     discovered_emoji = text.scan(/:([\w\d\-\_]+):/).flatten.map(&:to_sym).uniq
     found = discovered_emoji.each_with_object({}) do |emoji, found|
@@ -13,12 +13,18 @@ module EmojiSub
     end.compact
 
     found.each do |shortcode, unicode|
-      text.gsub!(/:#{shortcode}:/, "&#x#{unicode}")
+      text.gsub!(/:#{shortcode}:/, Array(unicode).map { |u| "&#x#{u}" }.join(''))
     end
     
     text
   end
-  module_function :emoji_sub
 
+  def emoji_definitions
+    m = YAML.load(File.read(EMOJI_MAPPING_YAML)).values
+    @emoji_definitions ||= m.inject({}) { |a,memo| memo.merge(a) }
+    
+    #@emoji_definitions ||= m.collect(&:values).flatten
+  end
+  module_function :emoji_sub, :emoji_definitions
 
 end
