8d1163379222bfb9a0c2c58303c95d20a4621c9e
Aaron Hill <armahillo@gmail.com>
2020-08-21 21:32:00 -0400

    Adds ability to provide extra mappings

--- lib/emoji_sub.rb
+++ lib/emoji_sub.rb
@@ -1,16 +1,12 @@
 require "emoji_sub/version"
 require 'YAML'
 
-puts "Module!"
-puts Dir.pwd
-
 module EmojiSub
   EMOJI_MAPPING_YAML = "data/emoji.yaml"
-
-  class Error < StandardError; end
   
-  def emoji_sub(text)
-    known_emoji = YAML.load(File.read(EMOJI_MAPPING_YAML))
+  def emoji_sub(text, additional_mappings = {})
+    known_emoji = YAML.load(File.read(EMOJI_MAPPING_YAML)).merge(additional_mappings)
+
     discovered_emoji = text.scan(/:([\w\d\-\_]+):/).flatten.map(&:to_sym).uniq
     found = discovered_emoji.each_with_object({}) do |emoji, found|
       found[emoji] = known_emoji.fetch(emoji,nil)
@@ -26,6 +22,3 @@ module EmojiSub
 
 
 end
-
-#  TEXT = 
-# p emoji_sub(TEXT)
--- spec/emoji_sub_spec.rb
+++ spec/emoji_sub_spec.rb
@@ -4,13 +4,22 @@ RSpec.describe EmojiSub do
   end
 
   describe "emoji_sub" do
-    subject { EmojiSub.emoji_sub(text) }
+    subject { EmojiSub.emoji_sub(text, additional_mapping) }
+    let(:additional_mapping) { {} }
 
     context "with one or more different emoji" do
       let(:text) { 'This is testing text :100:. It is great :slightly_smiling_face:.' }
 
       it "replaces the shortcodes with their corresponding unicode" do
         expect(subject).to eq('This is testing text &#x1F4AF. It is great &#x1F642.')
+      end
+
+      context "when provided with an optional parameter of additional mappings" do
+        let(:additional_mapping) { { '100': "ABCD" } }
+
+        it "overrides existing emoji" do
+          expect(subject).to eq('This is testing text &#xABCD. It is great &#x1F642.')
+        end
       end
     end
 
@@ -27,6 +36,14 @@ RSpec.describe EmojiSub do
 
       it "leaves the shortcodes intact" do
         expect(subject).to eq('This is testing text &#x1F4AF :a_nonexistent_emoji:. It is great &#x1F642.')
+      end
+
+      context "when provided with an optional parameter of additional mappings" do
+        let(:additional_mapping) { { a_nonexistent_emoji: "ABCD" } }
+
+        it "overrides existing emoji" do
+          expect(subject).to eq('This is testing text &#x1F4AF &#xABCD. It is great &#x1F642.')
+        end
       end
     end
   end
