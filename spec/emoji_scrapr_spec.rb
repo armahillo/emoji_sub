602a94c8491a6fe67fe19e88e11f052f104b5f78
Aaron Hill <armahillo@gmail.com>
2020-08-22 11:08:13 -0400

    Small test refactor

--- spec/emoji_scrapr_spec.rb
+++ spec/emoji_scrapr_spec.rb
@@ -19,10 +19,12 @@ RSpec.describe EmojiScrapr do
   end
 
   describe "#scrape_emoji_page" do    
+    before do
+      stub_request('fake.html', response)
+    end
+
     context "when the page has a single shortcode and unicode pair" do
-      before do
-        stub_request('fake.html', emoji_entry_page[:basic])
-      end
+      let(:response) { emoji_entry_page[:basic] }
 
       it "responds with a hash of unicode and shortcodes" do
         result = subject.scrape_emoji_page('/fake.html')
@@ -33,9 +35,7 @@ RSpec.describe EmojiScrapr do
     end
 
     context "when the page has a hyphens in the shortcodes" do
-      before do
-        stub_request('fake.html', emoji_entry_page[:hyphens])
-      end
+      let(:response) { emoji_entry_page[:hyphens] }
 
       it "responds with a hash of unicode and shortcode" do
         result = subject.scrape_emoji_page('/fake.html')
@@ -46,9 +46,7 @@ RSpec.describe EmojiScrapr do
     end
 
     context "when the page has multiple shortcodes" do
-      before do
-        stub_request('fake.html', emoji_entry_page[:multiple_shortcodes])
-      end
+      let(:response) { emoji_entry_page[:multiple_shortcodes] }
 
       it "responds with a hash of unicode and shortcodes" do
         result = subject.scrape_emoji_page('/fake.html')
@@ -59,9 +57,7 @@ RSpec.describe EmojiScrapr do
     end
 
     context "when the page has multiple unicodes" do
-      before do
-        stub_request('fake.html', emoji_entry_page[:multiple_unicodes])
-      end
+      let(:response) { emoji_entry_page[:multiple_unicodes] }
 
       it "responds with a hash of unicode and shortcodes" do
         result = subject.scrape_emoji_page('/fake.html')
@@ -70,5 +66,9 @@ RSpec.describe EmojiScrapr do
         expect(result[:shortcodes]).to match_array([':shortcode_test_1:'])
       end
     end
+  end
+
+  describe "#scrape_categories_page" do
+
   end
 end
